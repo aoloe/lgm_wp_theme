a68092440699bbf9680b8f4f78f1e6c6303c0840
a.l.e <ale@ideale.ch>
2013-04-01 09:31:38 +0200

    add title in head

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
 <head>
-	<title>Libre graphics meeting, April 10-13 2013, Madrid</title>
+	<title><?= ucfirst($wp_query->queried_object->post_name) ?> / <?php bloginfo('name'); ?></title>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	<link rel="stylesheet" href="<? bloginfo('template_url') ?>/css/reset.css" type="text/css"/>
 	<link rel="stylesheet" href="<? bloginfo('template_url') ?>/css/fonts.css" type="text/css"/>
